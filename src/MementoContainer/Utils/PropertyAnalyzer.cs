350c09b9fd17dad8257836e4bc261cd25b8590c4
Diogo Castro <diogo.filipe.acastro@gmail.com>
2013-10-06 13:03:28 +0100

    Property analyzer searches for attributes on implemented interfaces

--- src/MementoContainer/Utils/PropertyAnalyzer.cs
+++ src/MementoContainer/Utils/PropertyAnalyzer.cs
@@ -100,19 +100,58 @@ namespace MementoContainer.Utils
 
         public IList<IPropertyAdapter> GetProperties(object obj)
         {
-            Type type = obj.GetType();
-            var props = type.GetTypeInfo().DeclaredProperties;
+            TypeInfo typeInfo = obj.GetType().GetTypeInfo();
+            var props = typeInfo.DeclaredProperties;
 
-            //Return all valid properties
-            if (type.GetTypeInfo().IsDefined(typeof(MementoClassAttribute)))
-            {
-                var validProps = props.Where(p => p.HasGetAndSet()).ToList();
+            //check if type has MementoClassAttribute
+            bool isMementoClass;
+            var validProps = LookupProperties(typeInfo, out isMementoClass);
+
+            if (isMementoClass)
                 return WrapProperties(validProps);
+
+            //lookup properties in the type's interfaces
+            var interfaces = typeInfo.ImplementedInterfaces;
+            var interfaceAnnotatedProps = interfaces.SelectMany(LookupProperties);
+
+            //filter annotated properties
+            var annotatedProps = props.Where(p =>
+                                p.IsDefined(typeof (MementoPropertyAttribute)) ||
+                                interfaceAnnotatedProps.Any(interfaceProp => interfaceProp.Name == p.Name))
+                         .ToList();
+            return ValidateProperties(annotatedProps);
+        }
+
+        private IList<PropertyInfo> LookupProperties(Type type)
+        {
+            bool unused;
+            return LookupProperties(type.GetTypeInfo(), out unused);
+        }
+
+        /// <summary>
+        /// Returns the type's annotated properties.
+        /// If the type has the MementoClass attribute defined, then all properties with get and set accessors are returned.
+        /// Otherwise, all properties with the MementoProperty attribute are returned.
+        /// </summary>
+        /// <param name="typeInfo"></param>
+        /// <param name="isMementoClass"></param>
+        /// <returns></returns>
+        private IList<PropertyInfo> LookupProperties(TypeInfo typeInfo, out bool isMementoClass)
+        {
+            IList<PropertyInfo> props;
+
+            if (typeInfo.IsDefined(typeof(MementoClassAttribute)))
+            {
+                isMementoClass = true;
+                props = typeInfo.DeclaredProperties.Where(p => p.HasGetAndSet()).ToList();
+            }
+            else
+            {
+                isMementoClass = false;
+                props = typeInfo.DeclaredProperties.Where(p => p.IsDefined(typeof(MementoPropertyAttribute))).ToList();
             }
 
-            //Return properties with the Memento attribute
-            var mementoProps = props.Where(p => p.IsDefined(typeof(MementoPropertyAttribute))).ToList();
-            return ValidateProperties(mementoProps);
+            return props;
         }
 
         /// <summary>
