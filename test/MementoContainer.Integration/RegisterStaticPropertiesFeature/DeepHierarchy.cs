bcd8612e6f3000b92bbc26868c87a0c4e3c4648c
Diogo Castro <diogo.filipe.acastro@gmail.com>
2013-10-07 14:05:39 +0100

    Integration tests for static properties

---
+++ test/MementoContainer.Integration/RegisterStaticPropertiesFeature/DeepHierarchy.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MementoContainer.Integration.RegisterStaticPropertiesFeature
+{
+    [TestFixture]
+    public class DeepHierarchy
+    {
+        [Test]
+        public void Test()
+        {
+            Article.Count = new Count {Number = 1};
+
+            var memento = Memento.Create()
+                                 .RegisterProperty(() => Article.Count.Number);
+
+            Article.Count.Number++;
+
+            memento.Restore();
+
+            Assert.AreEqual(1, Article.Count.Number);
+        }
+
+        private static class Article
+        {
+            public static Count Count { get; set; }
+        }
+
+        private class Count
+        {
+            public int Number { get; set; }
+            public DateTime LastUpdated { get; set; }
+        }
+    }
+}
