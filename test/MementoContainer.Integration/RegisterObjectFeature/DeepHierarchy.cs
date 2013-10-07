2a159dffd4da882f92256a324f5697b07cdee779
Diogo Castro <diogo.filipe.acastro@gmail.com>
2013-10-07 13:25:17 +0100

    Added integration tests for deep type/property hierarchies

---
+++ test/MementoContainer.Integration/RegisterObjectFeature/DeepHierarchy.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MementoContainer.Attributes;
+using NUnit.Framework;
+
+namespace MementoContainer.Integration.RegisterObjectFeature
+{
+    [TestFixture]
+    public class DeepHierarchy
+    {
+        [Test]
+        public void Test()
+        {
+            Author author = new Author {Name = "DCastro"};
+            Article article = new Article {Author = author};
+
+            var memento = Memento.Create()
+                                 .Register(article);
+
+            author.Name = "No one";
+            article.Author = new Author {Name = "No one"};
+
+            memento.Restore();
+
+            Assert.AreSame(author, article.Author);
+            Assert.AreEqual("DCastro", article.Author.Name);
+        }
+
+
+        private class Article
+        {
+            [MementoProperty]
+            public Author Author { get; set; }
+        }
+
+        private class Author
+        {
+            [MementoProperty]
+            public string Name { get; set; }
+        }
+    }
+}
---
+++ test/MementoContainer.Integration/RegisterPropertiesFeature/DeepHierarchy.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MementoContainer.Attributes;
+using NUnit.Framework;
+
+namespace MementoContainer.Integration.RegisterPropertiesFeature
+{
+    [TestFixture]
+    public class DeepHierarchy
+    {
+        [Test]
+        public void Test()
+        {
+            Author author = new Author { Name = "DCastro" };
+            Article article = new Article { Author = author };
+
+            var memento = Memento.Create()
+                                 .RegisterProperty(article, a => a.Author.Name);
+
+            article.Author = new Author { Name = "No one" };
+
+            memento.Restore();
+
+            Assert.AreEqual("DCastro", article.Author.Name);
+        }
+
+
+        private class Article
+        {
+            [MementoProperty]
+            public Author Author { get; set; }
+        }
+
+        private class Author
+        {
+            [MementoProperty]
+            public string Name { get; set; }
+        }
+    }
+}
