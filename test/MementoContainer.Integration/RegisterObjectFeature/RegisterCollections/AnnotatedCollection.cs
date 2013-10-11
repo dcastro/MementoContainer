98e05ecb2f226ef906053f1a6b9d5fa27fea9488
Diogo Castro <diogo.filipe.acastro@gmail.com>
2013-10-11 13:04:26 +0100

    Added integration tests for registering collections

---
+++ test/MementoContainer.Integration/RegisterObjectFeature/RegisterCollections/AnnotatedCollection.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using MementoContainer.Attributes;
+using NUnit.Framework;
+
+namespace MementoContainer.Integration.RegisterObjectFeature.RegisterCollections
+{
+    [TestFixture]
+    [Ignore("Not implemented yet.")]
+    public class AnnotatedCollection
+    {
+        [Test]
+        public void Test()
+        {
+            var ids = new List<int> {1, 2, 3};
+            var authors = new List<string> {"a", "b", "c"};
+            var article = new Article
+                {
+                    Ids = ids,
+                    Authors = authors
+                };
+
+            var memento = Memento.Create()
+                                 .Register(article);
+
+            article.Ids.Remove(2);
+            article.Ids.Add(4);
+
+            article.Authors.Remove("b");
+            article.Authors.Add("d");
+
+            memento.Restore();
+
+            CollectionAssert.AreEqual(new List<int> {1, 2, 3}, article.Ids);
+            CollectionAssert.AreEqual(new List<string> {"a", "c", "d"}, article.Authors);
+        }
+
+        class Article
+        {
+            [MementoCollection]
+            public List<int> Ids { get; set; }
+
+            public List<string> Authors { get; set; }
+        }
+    }
+}
---
+++ test/MementoContainer.Integration/RegisterObjectFeature/RegisterCollections/NullCollection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MementoContainer.Attributes;
+using NUnit.Framework;
+
+namespace MementoContainer.Integration.RegisterObjectFeature.RegisterCollections
+{
+    [TestFixture]
+    public class NullCollection
+    {
+        [Test]
+        public void Test()
+        {
+            var article = new Article
+                {
+                    Ids = null
+                };
+
+            var memento = Memento.Create()
+                                 .Register(article);
+
+            article.Ids = new List<int> {1};
+
+            memento.Restore();
+
+            CollectionAssert.AreEqual(new List<int> {1}, article.Ids);
+        }
+
+        class Article
+        {
+            [MementoCollection]
+            public List<int> Ids { get; set; }
+        }
+    }
+}
