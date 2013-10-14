1ab2aaaa939953b6df95e576d78c994bee719f57
Diogo Castro <diogo.filipe.acastro@gmail.com>
2013-10-14 16:45:28 +0100

    Added 'cascade' parameter to MementoCollection attribute

--- src/MementoContainer/Attributes/MementoCollectionAttribute.cs
+++ src/MementoContainer/Attributes/MementoCollectionAttribute.cs
@@ -13,5 +13,16 @@ namespace MementoContainer
     [AttributeUsage(AttributeTargets.Property)]
     public class MementoCollectionAttribute : Attribute
     {
+        internal bool Cascade { get; private set; }
+
+        /// <summary>
+        /// Specifies that this collection should be recorded within the memento container.
+        /// After <see cref="IMemento.Restore"/> is called, the collection will contain the same elements as it originally did.
+        /// </summary>
+        /// <param name="cascade">Specifies whether items in this collection should be added to the container as well.</param>
+        public MementoCollectionAttribute(bool cascade = true)
+        {
+            Cascade = cascade;
+        }
     }
 }
---
+++ test/MementoContainer.Integration/RegisterObjectFeature/RegisterCollections/NoCascading.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MementoContainer.Integration.RegisterObjectFeature.RegisterCollections
+{
+    [TestFixture]
+    public class NoCascading
+    {
+        [Test]
+        public void Test()
+        {
+            var author = new Author { Name = "DCastro" };
+            var article = new Article
+            {
+                Authors = new List<Author> { author }
+            };
+
+            var memento = Memento.Create()
+                                 .Register(article);
+            
+            author.Name = "No one";
+
+            memento.Restore();
+
+            CollectionAssert.AreEqual("No one", article.Authors.First().Name);
+        }
+
+        private class Article
+        {
+            [MementoCollection(false)]
+            public List<Author> Authors { get; set; }
+        }
+
+        [MementoClass]
+        private class Author
+        {
+            public string Name { get; set; }
+        }
+    }
+}
