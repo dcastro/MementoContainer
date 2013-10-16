03aee4137e6c29dc4dc6168eb4d39f9783f73f17
Diogo Castro <diogo.filipe.acastro@gmail.com>
2013-10-16 12:54:10 +0100

    Integration tests now use a custom SimpleCollection

--- test/MementoContainer.Integration/RegisterObjectFeature/RegisterCollections/RegisterCollectionItems.cs
+++ test/MementoContainer.Integration/RegisterObjectFeature/RegisterCollections/RegisterCollectionItems.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -18,7 +19,7 @@ namespace MementoContainer.Integration.RegisterObjectFeature.RegisterCollections
             var author = new Author {Name = "DCastro"};
             var article = new Article
                 {
-                    Authors = new List<Author> {author}
+                    Authors = new SimpleCollection<Author> {author}
                 };
 
             var memento = Memento.Create()
@@ -35,13 +36,74 @@ namespace MementoContainer.Integration.RegisterObjectFeature.RegisterCollections
         private class Article
         {
             [MementoCollection]
-            public List<Author> Authors { get; set; }
+            public SimpleCollection<Author> Authors { get; set; }
         }
 
         [MementoClass]
         private class Author
         {
             public string Name { get; set; }
+        }
+    }
+
+    public class SimpleCollection<T> : ICollection<T>
+    {
+        private T[] _arr = new T[15];
+        private int _count = 0;
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < _count; i++)
+            {
+                yield return _arr[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Add(T item)
+        {
+            if(_count == 15)
+                throw new Exception("The collection is at maximum capacity: items cannot be added.");
+
+            _arr[_count] = item;
+            _count++;
+        }
+
+        public void Clear()
+        {
+            _arr = new T[15];
+            _count = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            return _arr.Take(_count).Contains(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            _arr.Take(_count)
+                .ToArray()
+                .CopyTo(array, arrayIndex);
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count
+        {
+            get { return _count; }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return false; }
         }
     }
 }
